ae05cf8f4e0539e7cec326fe23b4726467c11114
13Haikal <13haikalmochammmad@gmail.com>
2026-04-20 12:33:19 +0700

    a

---
+++ praktikno5/mahasiswa.java
@@ -0,0 +1,9 @@
+public class Mahasiswa {
+    private String nama;
+    private int sks;
+
+    public void setNama(String n) { nama = n; }
+    public void setSks(int s) { sks = s; }
+    public String getNama() { return nama; }
+    public int getSks() { return sks; }
+}
